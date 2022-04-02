4e6bc3365677bbd45e2a9ad30d0605729c7ea68a
Ruth Amurao <72446116+amuraoo@users.noreply.github.com>
2022-04-02 20:28:28 +0400

    landing page

---
+++ landing_page.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Swapee</title>
+
+  </head>
+
+  <body>
+
+  <section id="section1">
+    <div class="container">
+        <!-- logo and brand name -->
+        <header class="head my-3">
+            <nav class="navbar navbar-expand-lg navbar-light head__custom-nav">
+                <a class="navbar-brand d-flex align-items-center" href="#">
+                    <img src="images/swapee_ic.png">
+                    <span>SWAPEE</span>
+                </a>
+
+                <!-- menu button -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span><img src="images/menu.png" alt=""></span>
+                </button>
+
+                <!-- navigation bar -->
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#section1">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#section2">SERVICES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#section3">FEATURES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#section4">TEAM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link join-us" style="color: white; background-color: #FEB005;" href="index.php">JOIN US</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    </div>
+  </section>
+    
+
+    <!-- HOME -->
+    <div class="container">
+        <div class="row custom-section d-flex align-items-center">
+            <div class="col-12 col-lg-4">
+                <h2>SWAP ANYWHERE</h2>
+                <h3>ONLINE!</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, quidem!</p>
+                <a href="#">Get Started!</a>
+            </div>
+            <div class="col-12 col-lg-8">
+                <img src="images/Online store _Isometric 1 (2).png" alt="Swapee banner">
+                    
+            </div>
+        </div>
+    </div>
+
+    <!-- SERVICES -->
+    <section id="section2">
+        <div class="container">
+            <div class="row custom-section d-flex align-items-center">
+
+                <div class="col-12 col-lg-8">
+                    <img src="images/Online Shopping_Isometric 1.png" alt="Swapee banner">
+                </div>
+
+                <div class="col-12 col-lg-4">
+                    <h3>Services we provide</h3>
+                    <p>Integer sit amet tortor iaculis, pulvinar orci ac, dapibus arcu. Nulla tempus vehicula est, vel aliquam enim pretium viverra. Ut et efficitur mi. Nunc libero mauris, facilisis quis purus eget, tempor scelerisque eros.</p>
+                </div> 
+            </div>
+        </div>
+    </section>
+
+    <!-- FEATURES -->
+    <section id="section3">
+        <div class="container">
+            <div class="row custom-section d-flex align-items-center">
+                <div class="col-12 col-lg-4">
+                    <h3>Some of our main features</h3>
+                    <p>Integer sit amet tortor iaculis, pulvinar orci ac, dapibus arcu. Nulla tempus vehicula est, vel aliquam enim pretium viverra. Ut et efficitur mi. Nunc libero mauris, facilisis quis purus eget, tempor scelerisque eros.</p>
+                </div> 
+                <div class="col-12 col-lg-8">
+                    <img src="images/Online payment_Isometric 1.png" alt="Swapee banner">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- TEAM -->
+    <section id="section4">
+        <div class="container">
+
+        <div id="team-head">
+            <h2>TEAM</h2>
+        </div>
+
+            <div class="team">
+                <!-- patrisha -->
+                <div class="pat">
+                    <img src="images/carterino.jpeg" alt="" srcset="">
+                    <div id="description">
+                        <h6>Patrisha Ortigas</h6>
+                        <p>Backend Developer</p>
+                    </div>
+                </div>
+
+                <!-- carter -->
+                <div class="cart">
+                    <img src="images/carterino.jpeg" alt="" srcset="">
+                    <div id="description">
+                        <h6>Trisha Catis</h6>
+                        <p>Frontend Developer</p>
+                    </div>
+                </div>
+
+                <!-- gabby -->
+                <div class="gab">
+                    <img src="images/carterino.jpeg" alt="" srcset="">
+                    <div id="description">
+                        <h6>Alysxandra Reyes</h6>
+                        <p>Frontend Developer</p>
+                    </div>
+                </div>
+                
+                <!-- ruth -->
+                <div class="ruth">
+                    <img src="images/carterino.jpeg" alt="" srcset="">
+                    <div id="description">
+                        <h6>Ruth Amurao</h6>
+                        <p>Frontend Developer</p>
+                    </div>
+                </div>
+
+                <!-- bianca -->
+                <div class="bia">
+                    <img src="images/carterino.jpeg" alt="" srcset="">
+                    <div id="description">
+                        <h6>Bianca Dalangin</h6>
+                        <p>Web Designer</p>
+                    </div>
+                </div>
+                
+                <!-- sarai -->
+                <div class="sari">
+                    <img src="images/carterino.jpeg" alt="" srcset="">
+                    <div id="description">
+                        <h6>Sarai Carreon</h6>
+                        <p>Web Designer</p>
+                    </div>
+                </div>
+
+            </div>
+           
+        </div>
+</section>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+</body>
+</html>
