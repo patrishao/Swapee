fed4e65235aa0e5473e9d3750ff3ed39b71e1d7d
cctrolley <102876769+cc-1205@users.noreply.github.com>
2022-04-03 20:50:15 +0400

    w bootstrap

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6 p-0 login-face-con">
+                <img src="images/login_face.png" alt="" class="login-face">
+            </div>
+            <div class="col-sm-6 p-10">
+                <div class="centerer">
+                    <h1 class="login-title">LOG IN</h1>
+                    <div class="log-in-container d-flex justify-content-center">
+                        <form action="" class="login-form">
+                            <input type="email" placeholder="Email" class="textbox-login">
+                            <input type="password" placeholder="Password" class="textbox-login">
+                            <p class="forgot-p">Forgot password?</p>
+                            <button type="submit" value="" class="btn-login"> 
+                                LOG IN
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
