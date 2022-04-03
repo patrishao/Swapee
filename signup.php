769c3aa743cf1fbc1d63ba93e1110092fd41e8f0
alysxandra <71811550+alysxandra@users.noreply.github.com>
2022-04-04 02:45:47 +0400

    signup page done ish

---
+++ signup.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGN UP</title>
+    <!-- css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- script -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/signup.css">
+
+</head>
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-sm-6">
+
+                <div class="form-container">
+                    <h1 class="signup-title">SIGN UP</h1>
+
+                    <form action="" id="">
+                        <div class="signup-container">
+                            <div class="signup-group">
+                                <input type="text" name="email" placeholder="Email">
+                            </div>
+                            <div class="signup-group">
+                                <input type="text" name="password" placeholder="Password">
+                            </div>
+                            <div class="signup-group">
+                                <input type="text" name="c-password" placeholder="Re-type password">
+                            </div>
+
+                            <div class="signup-group2">
+                                <input type="button" value="SIGN UP" class="signup-button">
+                            </div>
+                        </div>       
+                    </form>
+                </div>
+
+            </div>
+
+            <div class="col-sm-6">
+                <img src="images/signup-face.png" alt="" class="signup-face">
+            </div>
+
+        </div>
+        
+    </div>
+
+</body>
+</html>
