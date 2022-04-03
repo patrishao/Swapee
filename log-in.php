37d751b80d171112c9c77c461fa1b835a5b1377a
cctrolley <102876769+cc-1205@users.noreply.github.com>
2022-04-03 15:54:38 +0400

    added files

---
+++ log-in.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOG IN</title>
+
+     <link rel="stylesheet" href="log-in.css">
+
+</head>
+<body>
+    <div class="maincontainer">
+
+        <div class="leftcol-img">
+            
+        </div>
+
+            <div class="rightcol-textbox">
+                <div class="container-r">
+
+                    <h1 class="login">
+                        LOG IN
+                    </h1>
+
+                    <div class="form-container">
+                        <form action="" id="">
+                            <div class="login-container">
+
+                                <div class="log-in-textbox">
+                                    <input type="text" name="email">
+                                </div>
+                                <div class="log-in-textbox">
+                                    <input type="text" name="password" class="pass-login">
+                                </div>
+
+                            </div>
+
+                            <input type="button" value="" class="button-login">
+
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+    </div>
+</body>
+</html>
