b48d6aa466f40cc0659b4b8af08b37cae9df502b
cctrolley <102876769+cc-1205@users.noreply.github.com>
2022-05-17 01:07:22 +0400

    update pu

--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PROFILE</title>
 
       <!-- Bootstrap CSS -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -20,15 +20,33 @@
       <div class="row">
 
       <!-- left side -->
-        <div class="col-sm-6 post-side">
+        <div class="col-md-6 post-side">
           <img src="images\placeholder.jpg" alt="" class="post-placeholder">
           <img src="images\placeholder.jpg" alt="" class="post-placeholder">
           <img src="images\placeholder.jpg" alt="" class="post-placeholder">
         </div>
 
       <!-- right side (profile)-->
-      <div class="col-sm-6 profile-side">
-        profile side
+      <div class="col-md-6 profile-side">
+
+        <div class="content-container">
+
+              <div class="container-firstpt">
+             <img src="images/carterino.jpeg" alt="" class="profile-pic">
+             <div class="name-user">
+                    <div class="name">display name po</div>
+                    <div class="username">@usernamepoito</div>
+              </div>
+              </div>
+              <div class="bio">bio po it maglagay ng kung anong ano dine thank u</div>
+              <div class="container-secondpt">
+                <div class="location">dubai baket</div>
+                <div class="rating"></div>
+                <div class="prof-buttons"></div>
+              </div>
+
+          </div>
+
       </div>
 
       </div>
