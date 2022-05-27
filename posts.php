6a82bd76c4adfc62df390e97ea484814bc56f2fe
Patrisha Ortigas <patrishagortigas@gmail.com>
2022-05-28 00:44:14 +0400

    fixed post sliders & signup

--- posts.php
+++ posts.php
@@ -7,13 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
     <!-- For the carousel pictures -->
-    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+
 
     <!-- CUSTOM CSS -->
     <link rel="stylesheet" href="css/posts.css">
@@ -31,16 +32,33 @@
 
     <div class="main-container">
 
-        <div class="posts-container">
 
-            <div class="pic-container" id="slider">
-                <figure class="pic-main">
-                    <img src="images/uploaded-imgs/detergentessentials.png">
-                    <img src="images/uploaded-imgs/detergentessentials.png">
-                    <img src="images/uploaded-imgs/detergentessentials.png">
-                    <img src="images/uploaded-imgs/detergentessentials.png">
-                </figure>
+        <!-- CHANGES BY CHAY -->
+
+
+        <div class="pic-holder ps-4 pe-4">
+
+
+
+
+            <div class="prev-arrow arrow">
+                <button>left</button>
             </div>
+
+            <div class="container photo-container p-5">
+                <img src="images/uploaded-imgs/chanelmakeup.jpg" class="photos">
+                <img src="images/uploaded-imgs/diorlipstic.jpg" class="photos">
+            </div>
+
+            <div class="right-arrow arrow">
+                <button>right</button>
+            </div>
+        </div>
+
+
+
+
+        <div class="posts-container ">
 
             <div class="text-container">
 
@@ -51,7 +69,8 @@
 
                 <div class="desc-cont">
                     <p class="desc-title">Description</p>
-                    <p class="desc-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis vehicula velit. Suspendisse potenti. Ut tempor sem et pellentesque dapibus. </p>
+                    <p class="desc-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis vehicula
+                        velit. Suspendisse potenti. Ut tempor sem et pellentesque dapibus. </p>
                 </div>
 
                 <div class="seller-cont">
@@ -61,21 +80,26 @@
                         <div class="seller-desc2">
                             <p class="prof-name">Kaya Todei</p>
                             <p class="prof-user">@dimosyur</p>
-                            <p class="prof-det"><img src="images/icons/loc-icon.svg" alt=""> Sharjah <img src="images/icons/bi_star.svg" alt=""> 2.1/5</p>
+                            <p class="prof-det"><img src="images/icons/loc-icon.svg" alt=""> Sharjah <img
+                                    src="images/icons/bi_star.svg" alt=""> 2.1/5</p>
                         </div>
                     </div>
                 </div>
 
                 <div class="status-cont">
-                    <p class="status-title">Status: </p> <p class="status-stat">&nbsp Wanted</p>
+                    <p class="status-title">Status: </p>
+                    <p class="status-stat">&nbsp Wanted</p>
                 </div>
 
                 <div class="send-cont">
-                    <p class="send-title">Send seller a message</p> 
+
+
+                    <p class="send-title">Send seller a message</p>
                     <form class="send-message" action="" method="POST">
-                        <input class="send-input" type="text" name="sendquestion" value="" placeholder="Hi, is this available?"> <br>
+                        <input class="send-input" type="text" name="sendquestion" value=""
+                            placeholder="Hi, is this available?"> <br>
                         <input class="send-btn" type="submit" name="send" value="Send">
-                    </form> 
+                    </form>
                 </div>
 
 
@@ -91,7 +115,30 @@
             <img src="images/uploaded-imgs/detergentessentials.png">
         </figure>
     </div> -->
-    
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+
+    </script>
+
+
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+
+    <script>
+    $('.photo-container').slick({
+        infinite: true,
+        prevArrow: $('.prev-arrow'),
+        nextArrow: $('.right-arrow'),
+        slidesToShow: 1,
+        slidesToScroll: 1
+
+    });
+    </script>
+
 
 </body>
 
--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,14 +9,23 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="samp.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- script -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+
+    <!-- bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+
+
 
 </head>
+
 <body>
 
     <!-- outside container -->
@@ -24,39 +34,98 @@
         <!-- inside container -->
         <div class="row">
 
-        <div class="logo">
-            <nav>
-                <img src="images/logo.png" id="logo1" alt="" srcset="">
-            </nav>
-            
-        </div>
-    
+            <div class="logo">
+                <nav>
+                    <img src="images/logo.png" id="logo1" alt="" srcset="">
+                </nav>
+
+            </div>
+
             <!-- right side  -->
             <div class="col-sm-6 p-10">
                 <div class="centerer">
                     <h1 class="login-title">SIGN UP</h1>
                     <div class="log-in-container d-flex justify-content-center">
-                        <form action="" class="login-form">
-                            <input type="email" name="email "placeholder="Email" class="textbox-login">
-                            <input type="password" name="password" placeholder="Password" class="textbox-login">
-                            <input type="password" name="re-type password" placeholder="Re-type Password" class="textbox-login">
+                        <form action="" class="login-form" method="post">
+
+
+                            <input type="email" name="email" placeholder="Email" class="textbox-login">
+
+                            <input type="text" name="firstName" placeholder="First name" class="textbox-login">
+
+
+                            <input type="text" name="lastName" placeholder="Last name" class="textbox-login">
+
+                            <input type="text" name="username" placeholder="Username" class=" textbox-login">
+
+
+
+                            <div class="pass-container w-100">
+                                <input type="password" name="password" placeholder="Password" class="textbox-login"
+                                    id="password1">
+                                <i class=" bi bi-eye-slash togglePass" id="togglePassword1"></i>
+                            </div>
+
+
+                            <div class="pass-container w-100">
+                                <input type="password" name="c-password" placeholder="Re-type Password"
+                                    class="textbox-login" id="password2">
+                                <i class=" bi bi-eye-slash togglePass" id="togglePassword2"></i>
+                            </div>
+
+
+
+
                             <a href="#" class="forgot-p">Forgot password?</a>
-                            <button type="submit" value="" class="btn-login"> 
+
+                            <?php require_once('private/signup-data.php') ?>
+
+
+                            <button type="submit" value="" class="btn-login" name="signUp">
                                 SIGN UP
                             </button>
                         </form>
-                       
+
                     </div>
                     <div class="text-login">Don't have an account? <a href="login.php" class="">Sign in here!</a></div>
                 </div>
             </div>
 
-                        <!-- right side -->
-                        <div class="col-sm-6 p-0 login-face-con">
+            <!-- right side -->
+            <div class="col-sm-6 p-0 login-face-con">
                 <img src="images/reg_face.png" alt="" class="login-face">
             </div>
 
         </div>
+
+        <script>
+        const togglePassword = document.querySelector("#togglePassword1");
+        const password = document.querySelector("#password1");
+
+        togglePassword.addEventListener("click", function() {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
+
+        const togglePassword2 = document.querySelector("#togglePassword2");
+        const password2 = document.querySelector("#password2");
+
+        togglePassword2.addEventListener("click", function() {
+            // toggle the type attribute
+            const type = password2.getAttribute("type") === "password" ? "text" : "password";
+            password2.setAttribute("type", type);
+
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
+        </script>
     </div>
+
+
 </body>
+
 </html>
