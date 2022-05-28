e33f5b925e5a3d97128427cc39e85955c7d80535
Patrisha Ortigas <patrishagortigas@gmail.com>
2022-05-28 23:37:56 +0400

    messages done :crying_cat_face:

--- message.php
+++ message.php
@@ -7,146 +7,289 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MESSAGES</title>
     <link rel="stylesheet" type="text/css" href="css/message.css" />
+    <!-- <meta http-equiv="refresh" content="10" /> -->
 </head>
 
 <body>
 
-    <!-- sweetalert 
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="sweetalert2.all.min.js"></script>
+    <?php include 'navbar.php';
+    require_once 'private/includes/connection.php';
+    require_once 'private/includes/functions.php';
+    require_once 'private/message-handler.php';
 
-    <script>
-        Swal.fire({
-            icon: 'error',
-            title: 'Sorry!',
-            text: 'This feature is still coming soon, please wait!',
+    $isCurrentMsg = false;
 
-        }).then(function() {
-            window.location.href = "index.php";
-        })
-    </script>
-        -->
-    <?php include 'navbar.php' ?>
 
+    ?>
+
+
+    <!-- TODO: PUT #CHATBOX ON THE LINK -->
     <div class="container">
 
-        <div class="titles">
-            <svg class="" xmlns="http://www.w3.org/2000/svg" width="35" height="31" viewBox="0 0 35 31" fill="none">
-                <path d="M34.0504 9.1751C34.0504 11.383 33.1259 13.5005 31.4802 15.0618C29.8346 16.623 27.6027 17.5001 25.2754 17.5001C22.9481 17.5001 20.7162 16.623 19.0705 15.0618C17.4249 13.5005 16.5004 11.383 16.5004 9.1751C16.5004 6.96717 17.4249 4.84967 19.0705 3.28843C20.7162 1.72719 22.9481 0.850098 25.2754 0.850098C27.6027 0.850098 29.8346 1.72719 31.4802 3.28843C33.1259 4.84967 34.0504 6.96717 34.0504 9.1751ZM26.2504 5.4751C26.2504 5.22977 26.1477 4.9945 25.9648 4.82102C25.782 4.64755 25.534 4.5501 25.2754 4.5501C25.0168 4.5501 24.7688 4.64755 24.586 4.82102C24.4031 4.9945 24.3004 5.22977 24.3004 5.4751V8.2501H21.3754C21.1168 8.2501 20.8688 8.34755 20.686 8.52102C20.5031 8.6945 20.4004 8.92977 20.4004 9.1751C20.4004 9.42042 20.5031 9.6557 20.686 9.82917C20.8688 10.0026 21.1168 10.1001 21.3754 10.1001H24.3004V12.8751C24.3004 13.1204 24.4031 13.3557 24.586 13.5292C24.7688 13.7026 25.0168 13.8001 25.2754 13.8001C25.534 13.8001 25.782 13.7026 25.9648 13.5292C26.1477 13.3557 26.2504 13.1204 26.2504 12.8751V10.1001H29.1754C29.434 10.1001 29.682 10.0026 29.8648 9.82917C30.0477 9.6557 30.1504 9.42042 30.1504 9.1751C30.1504 8.92977 30.0477 8.6945 29.8648 8.52102C29.682 8.34755 29.434 8.2501 29.1754 8.2501H26.2504V5.4751ZM30.1504 25.8251V18.2401C30.845 17.9033 31.4988 17.4958 32.1004 17.0246V25.8251C32.1004 27.0517 31.5868 28.2281 30.6725 29.0955C29.7583 29.9628 28.5183 30.4501 27.2254 30.4501H5.77539C4.48246 30.4501 3.24248 29.9628 2.32824 29.0955C1.41401 28.2281 0.900391 27.0517 0.900391 25.8251V11.0251C0.900391 9.79847 1.41401 8.62209 2.32824 7.75473C3.24248 6.88737 4.48246 6.4001 5.77539 6.4001H14.954C14.7736 7.00482 14.6528 7.62416 14.5933 8.2501H5.77539C4.99963 8.2501 4.25565 8.54246 3.7071 9.06288C3.15856 9.58329 2.85039 10.2891 2.85039 11.0251V11.5838L16.5004 19.2021L19.3103 17.6333C19.9441 18.0347 20.6246 18.3752 21.3442 18.6453L16.9957 21.0724C16.8713 21.1417 16.7323 21.184 16.5887 21.1961C16.445 21.2083 16.3003 21.19 16.165 21.1427L16.0051 21.0724L2.85039 13.7316V25.8251C2.85039 26.5611 3.15856 27.2669 3.7071 27.7873C4.25565 28.3077 4.99963 28.6001 5.77539 28.6001H27.2254C28.0011 28.6001 28.7451 28.3077 29.2937 27.7873C29.8422 27.2669 30.1504 26.5611 30.1504 25.8251Z" fill="white" />
-            </svg>
-        </div>
 
-
+        <?php if ($user_to != "new") { ?>
 
 
         <div class="contents">
 
             <div class="chat-list">
 
-                <div class="specific-chat">
-                    <div class="specific-user">
 
-                        <div class="identifier">
-                            <img class="user-img1" src="images\gabby.jpeg">
+                <?php
 
-                            <div class="names1">
-                                <h1 class="name-post1">Name </h1>
-                                <h2 class="msg-post1">hello pwede pa swap nuba </h1>
+                    // getting the conversations of user
+                    $convos = array();
+
+                    $getConvosQuery = mysqli_query($connection, "SELECT user_toID, user_fromID FROM messages WHERE user_toID = '$userLoggedIn' OR user_fromID = '$userLoggedIn'");
+
+
+                    while ($row = mysqli_fetch_array($getConvosQuery)) {
+                        // push wherever the user logged in is
+                        $user_to_push = $row['user_toID'] != $userLoggedIn ? $row['user_toID'] : $row['user_fromID'];
+
+                        if (!in_array($user_to_push, $convos)) {
+                            array_push($convos, $user_to_push);
+                        }
+                    }
+
+
+                    foreach ($convos as $username) {
+
+                        $user_found2 = getFirstLastName($connection, $username);
+                        $latest_message_details = getLatestMsg($userLoggedIn, $username, $connection);
+
+                        if (strlen($latest_message_details) > 50) {
+                            $lastConvo = substr($latest_message_details, 0, 50) . "...";
+                        } else {
+                            $lastConvo = $latest_message_details;
+                        }
+
+
+
+                    ?>
+
+                <a href="message.php?u=<?php echo $username; ?>">
+
+                    <div class="specific-chat <?php echo  $username == $user_to  ? "active" : ""; ?> ">
+                        <div class="specific-user">
+
+                            <div class="identifier">
+
+
+                                <img class="user-img1"
+                                    src="images/uploaded-imgs/<?php echo getUserImg($connection,  $username) ?>">
+
+
+                                <div class="names1">
+                                    <h1 class="name-post1"><?php echo  $user_found2 ?> </h1>
+                                    <h2 class="msg-post1"> <?php echo $lastConvo ?> </h1>
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="time">
-                            <p>1h</p>
-                        </div>
-
-                    </div>
-
-                </div>
-            <!--
-                <a href="smaller-message.php" class="specific-chat-clickable">
-                    <div class="specific-user">
-
-                        <div class="identifier">
-                            <img class="user-img1" src="images\gabby.jpeg">
-
-                            <div class="names1">
-                                <h1 class="name-post1">Name </h1>
-                                <h2 class="msg-post1">hello pwede pa swap nuba </h1>
-                            </div>
-                        </div>
 
                             <div class="time">
                                 <p>1h</p>
                             </div>
 
                         </div>
-             
+
+                    </div>
 
 
-                   </a> -->
+
+                    <?php } ?>
+
+                </a>
+
+
+
 
             </div>
 
 
-            <div class="chatbox">
 
-                <div class="user">
-                    <img class="user-img" src="images\gabby.jpeg">
 
-                    <div class="names">
-                        <h1 class="name-msg">Username </h1>
+            <div class="chatbox" id="chatbox">
+
+
+                <!-- linking the users to their profile page by passing their id to link -->
+                <a href="profile.php?u-id=<?php echo $user_to; ?>" class="linkprof">
+                    <div class="user">
+                        <img class="user-img"
+                            src="images/uploaded-imgs/<?php echo getUserImg($connection, $user_to) ?>">
+
+                        <div class="names">
+                            <h1 class="name-msg">
+                                <?php echo  $user_to_details = getFirstLastName($connection, $user_to); ?>
+                            </h1>
+                        </div>
                     </div>
+                </a>
+
+                <?php
+
+
+
+
+
+                    $updateQuery = mysqli_query($connection, "UPDATE messages SET isViewed = 1");
+
+                    //retrieving the msgs between the two
+                    $getMsgQuery = mysqli_query($connection, "SELECT * FROM messages WHERE user_toID = $userLoggedIn AND
+                user_fromID = $user_to OR user_fromID = $userLoggedIn AND user_toID = $user_to");
+
+
+
+
+
+
+                    ?>
+
+                <div class="inbox" id="scrollto">
+
+                    <?php
+
+
+
+
+
+
+
+
+
+
+                        while ($row = mysqli_fetch_assoc($getMsgQuery)) {
+
+
+                            $receiver = $row['user_toID'];
+                            $sender =  $row['user_fromID'];
+                            $body = $row['body'];
+
+
+
+
+
+
+                            if ($sender == $userLoggedIn) {
+
+
+
+                        ?>
+
+
+                    <div class="replier" id="">
+
+                        <img class="replier-img" src="images\<?php echo getUserImg($connection, $userLoggedIn) ?>">
+                        <p class="replier-text"><?php echo $body ?> </p>
+                    </div>
+
+
+
+                    <?php } else {
+
+
+                            ?>
+
+
+
+                    <div class="chatter ">
+
+                        <img class="chatter-img" src="images\<?php echo getUserImg($connection, $user_to) ?>">
+                        <p class="chat-text"><?php echo $body ?></p>
+                    </div>
+
+                    <?php  }
+                        } ?>
+
                 </div>
 
-                <div class="inbox">
-
-                    <div class="chatter">
-                        <img class="chatter-img" src="images\gabby.jpeg">
-                        <p class="chat-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-
-                    </div>
-
-                    <div class="replier">
-                        <img class="replier-img" src="images\carterino.jpeg">
-                        <p class="replier-text"> Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur </p>
-                    </div>
-
-                    <div class="chatter">
-                        <img class="chatter-img" src="images\gabby.jpeg">
-                        <p class="chat-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-                    </div>
-
-                    <div class="replier">
-                        <img class="replier-img" src="images\carterino.jpeg">
-                        <p class="replier-text"> Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur </p>
-                    </div>
-
-                    <div class="chatter">
-                        <img class="chatter-img" src="images\gabby.jpeg">
-                        <p class="chat-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-                    </div>
 
 
-                </div>
+                <form action="" class="form" method="post">
 
-                <form action="#" class="typing-area" class="form">
-
-                    <textarea name="txt-msg" class="txt-msg" placeholder="Enter your message here.."></textarea>
+                    <textarea name="txtMsg" class="txt-msg" placeholder="Enter your message here.." id="txt"></textarea>
 
 
-                    <button type="submit">
+                    <button type="submit" name="sendMsg" id="submit">
                         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none">
-                            <path d="M9.91245 12H4.00045L2.02345 4.13505C2.01079 4.08934 2.00308 4.0424 2.00045 3.99505C1.97845 3.27405 2.77245 2.77405 3.46045 3.10405L22.0004 12L3.46045 20.896C2.78045 21.223 1.99645 20.737 2.00045 20.029C2.00247 19.9658 2.01359 19.9031 2.03345 19.843L3.50045 15" stroke="#5CACA9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                                d="M9.91245 12H4.00045L2.02345 4.13505C2.01079 4.08934 2.00308 4.0424 2.00045 3.99505C1.97845 3.27405 2.77245 2.77405 3.46045 3.10405L22.0004 12L3.46045 20.896C2.78045 21.223 1.99645 20.737 2.00045 20.029C2.00247 19.9658 2.01359 19.9031 2.03345 19.843L3.50045 15"
+                                stroke="#5CACA9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </button>
                 </form>
 
 
 
+                <script>
+                var scroll = document.getElementById("scrollto");
+                scroll.scrollTop = scroll.scrollTop = 9999999;
+
+                const tx = document.getElementsByTagName("textarea");
+                var value = 0;
+
+
+                for (let i = 0; i < tx.length; i++) {
+
+
+
+                    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+
+
+                    tx[i].addEventListener("input", OnInput, false);
+
+
+
+
+
+                }
+
+                function OnInput() {
+
+
+                    // if the scroll height is less than the default size
+                    if (this.scrollHeight <= this.offsetHeight) {
+
+                        // reset the height  to default
+                        this.setAttribute('style', '' + "px;overflow-y:hidden;");
+                        this.style.height = (this.scrollHeight) + "px";
+
+
+
+
+                        // if the scroll height is greater than the default size
+                    } else {
+                        this.style.height = this.offsetHeight;
+                        this.style.height = (this.scrollHeight) + "px";
+
+                    }
+                }
+                </script>
+
 
 
             </div>
+
+
+
         </div>
+    </div>
+    <?php } else { ?>
+
+
+    <div class="no-msg-container">
+        <h2 class="no-msg">No messages yet</h2>
+        <p>Start a message by swapping with a user!</p>
+        <a href="index.php">Go to Home Page</a>
+        <?php } ?>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+
+    <!-- <script src="js/index.js"></script> -->
+
 </body>
 
 </html>
---
+++ private/message-handler.php
@@ -0,0 +1,57 @@
+<?php
+
+include_once 'private/includes/current-user-details.php';
+
+$userLoggedIn = $userID;
+
+
+
+// getting the user ID from the link
+if (isset($_GET['u'])) {
+    $user_to = $_GET['u'];
+    $isCurrentMsg = true;
+}
+
+
+// if not, get the most recent user 
+else {
+
+    $user_to = getMostRecentUser($connection, $userLoggedIn);
+    $isCurrentMsg = true;
+
+
+
+    // checks if the user has sent a message to anyone yet
+    if ($user_to == false) {
+        $user_to = "new";
+    }
+}
+
+
+
+// count database 
+
+
+
+// semdomg the message if there are values
+if (isset($_POST['sendMsg'])) {
+
+    $txtMsg = $_POST['txtMsg'];
+    $date = date("Y-m-d H:i:s");
+
+    if (empty($txtMsg)) {
+        echo "Msg is empty";
+    } else {
+
+
+
+
+        // cleaning the text
+        $txtMsg = cleanInput($connection, $txtMsg);
+
+
+        // inserting the query to the database, and making sure that the sender or userfrom is the userloggedin
+        $insertMsgQuery = mysqli_query($connection, "INSERT INTO messages (user_toID, user_fromID, body, date)VALUES ('$user_to', '$userLoggedIn', '$txtMsg', '$date')");
+        header("Location: message.php#chatbox");
+    }
+}
