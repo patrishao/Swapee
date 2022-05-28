7b247ecf686a7d38904fe11a1df60b4d74d44854
alysxandra <71811550+alysxandra@users.noreply.github.com>
2022-05-29 02:56:17 +0400

    signup back

---
+++ add-post.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="css/add-post.css">
+
+    <title>Swapee</title>
+</head>
+
+<body>
+
+    <div class="top-container">
+        <a href="posts.php" class="back-link"><img src="images/icons/back-arrow.svg" alt=""></a>
+        <p class="new-title">Create new post</p>
+        <button class="post-btn">Post</button>
+    </div>
+
+</body>
+
+</html>
