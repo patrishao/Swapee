a216e2f67e3e6c2f0d48533242c9cbd70e7d365f
cctrolley <102876769+cc-1205@users.noreply.github.com>
2022-05-29 06:27:43 +0400

    lets goafawoaogoaw

--- profile.php
+++ profile.php
@@ -167,8 +167,10 @@
                                                         } ?></p>
                         </div>
 
-                        <div class="rating"> <img src="images\icons\bi_star.png" alt="" class="rating-img">
+                        <div class="rating" onclick="myRating()">
+                             <img src="images\icons\bi_star.png" alt="" class="rating-img">
                             <p class="rating-details"><?php echo $db_rating; ?> / 5</p>
+
                         </div>
 
 
@@ -184,12 +186,40 @@
 
                     </div>
 
+                        <div class="rating-box" id="id-box">
+                        <p class="" style="align-self:center">Rate this user:</p>
+                            <div class="rate"> 
+                                <input type="radio" id="star5" name="rate" value="5" />
+                                <label for="star5" title="text">5 stars</label>
+                                <input type="radio" id="star4" name="rate" value="4" />
+                                <label for="star4" title="text">4 stars</label>
+                                <input type="radio" id="star3" name="rate" value="3" />
+                                <label for="star3" title="text">3 stars</label>
+                                <input type="radio" id="star2" name="rate" value="2" />
+                                <label for="star2" title="text">2 stars</label>
+                                <input type="radio" id="star1" name="rate" value="1" />
+                                <label for="star1" title="text">1 star</label>
+                            </div>
+                        </div>
+
                 </div>
 
             </div>
 
         </div>
     </div>
+    <script>
+        function myRating(){
+            var rating = document.getElementById("id-box");
+            if (rating.style.display === "none"){
+                rating.style.display = "flex";
+            } else {
+                rating.style.display = "none";
+            }
+
+        }
+        </script>
+
 </body>
 
 </html>
