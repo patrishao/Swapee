4ab4b33abd59861c8e93c725b836706d8574fcd4
alysxandra <71811550+alysxandra@users.noreply.github.com>
2022-05-31 01:02:41 +0400

    add a post done!

--- add-post.php
+++ add-post.php
@@ -18,10 +18,73 @@
 
 <body>
 
-    <div class="top-container">
+    <!-- <div class="top-container">
         <a href="posts.php" class="back-link"><img src="images/icons/back-arrow.svg" alt=""></a>
         <p class="new-title">Create new post</p>
         <button class="post-btn">Post</button>
+    </div> -->
+
+    <div class="main-container">
+
+        <div class="top-container">
+            <a href="posts.php" class="back-link"><img src="images/icons/back-arrow.svg" alt=""></a> 
+            <p class="new-title">Create new post</p>
+            <a class="post-btn"><button>Post</button></a>
+        </div>
+
+        <div class="below">
+            <div class="upload-container">
+                <div class="bg">
+                    
+                </div>
+                <div class="upload-overlay">
+                    
+                    <input type="file" id="file">
+                    <label for="file"><img src="images/icons/upload.svg" class="upload-icon" alt=""></label>
+                </div>
+            </div>
+
+            <!-- middle border -->
+            <div class="vertical"></div>
+
+            <div class="det-container">
+
+                <div class="det-cont">
+                    <form class="input-details" action="" method="POST">
+                        <div class="details-group">
+                            <input type="text" name="title" value="" placeholder="TITLE"> <br><br>
+                        </div>
+
+                        <div class="details-group">
+                            <input list="categories" name="category" placeholder="CATEGORY"> <br><br>
+                            <datalist id="categories">
+                                <option value="Home">
+                                <option value="Self-care">
+                                <option value="Beauty">
+                                <option value="Electronics">
+                                <option value="Supermarket">
+                                <option value="Sports">
+                                <option value="Laptops">
+                                <option value="Toys">
+                                <option value="Kitchen">
+                                <option value="Baby">
+                                <option value="Mobiles">
+                                <option value="Fashion">
+                                <option value="Instruments">
+                                <option value="Books">
+                                <option value="Others">
+                            </datalist>
+                        </div>
+
+                        <div class="details-group">
+                            <textarea name="desc" placeholder="DESCRIPTION" rows="1"></textarea> <br><br>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 
 </body>
